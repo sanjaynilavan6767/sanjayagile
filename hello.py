5f50e0cd08f76973e42263f8e0a6b1b580c23788
sanjaynilavan6767 <you@example.com>
2026-01-07 23:34:58 +0530

    before fetch

--- hello.py
+++ hello.py
@@ -3,4 +3,7 @@ def addbook():
     print("book")
     print("book added")
 def loginuser():
+<<<<<< HEAD (CURRENT CHANGE)    
     print("user has logged in successfully")    
+    print("user logged in")
+>>>>>> LOGIN-FEATURE (INCOMING CHANGE)
